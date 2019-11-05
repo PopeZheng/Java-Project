b930b9deefe312d0e9063b9d402cc2dcb2e983af
Pope Zheng <55305625+PopeZheng@users.noreply.github.com>
2019-11-05 22:04:16 +0800

    Update SolutionOfQuadraticEquation.java

--- SolutionOfQuadraticEquation.java
+++ SolutionOfQuadraticEquation.java
@@ -1,12 +1,11 @@
 import java.util.*;
 public class HelloWorldGraphics {
-
 	public static void main(String[] args) 
 	{
 		System.out.println("You are goning to use this to solve an equation"
 				+ "and its style is ax^2+bx+c=0\n"
 				+ "Please input a,b,c ");
-		double a,b,c,d,x1,x2,x;
+		double a,b,c,d,x1,x2,x,rp,ip;
 		char i;
 		Scanner sc= new Scanner(System.in);
 		do
@@ -18,21 +17,32 @@ public class HelloWorldGraphics {
 		System.out.print("c:");
 		c=sc.nextDouble();
 		d=b*b-4*a*c;
-		if (d<0)
+		if (Math.abs(a)<1e-6)
 		{
-			System.out.println("This equation has no solution");
-		}
-		else if (d==0)
-		{
-			x=-(b/(2*a));
-			System.out.println("The solutionS of this equation is:x1=x2="+x);
+			System.out.println("It's not a quadractic equation");
 		}
 		else
 		{
-			x1=(-b+Math.sqrt(d))/(2*a);
-			x2=(-b-Math.sqrt(d))/(2*a);
-			System.out.println("The solution of this equation are:x1="+x1+",x2="+x2);
+		    if (d<0)
+		    {
+		    	rp=-b/(2*a);
+		    	ip=Math.sqrt(-d)/(2*a);
+			    System.out.println("The solution of this equation are:"
+			    		+ "x1="+rp+"+"+ip+"i"
+			    		+ ",x2="+rp+"+"+ip+"i");
+		    }
+		    else if (d==0)
+		    {
+		    	x=-(b/(2*a));
+		    	System.out.println("The solutionS of this equation is:x1=x2="+x);
+		    }	
+		    else
+		    {
+		    	x1=(-b+Math.sqrt(d))/(2*a);
+		    	x2=(-b-Math.sqrt(d))/(2*a);
+		    	System.out.println("The solution of this equation are:x1="+x1+",x2="+x2);
 			
+		    }
 		}
 		System.out.print("Do you want to use me again?(T/F)");
 		i=sc.next().charAt(0);
